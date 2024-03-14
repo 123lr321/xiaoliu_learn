42091b6d52bc1c27002e6f5eb36ce97edf47a51e
liurun <1875281348@qq.com>
2024-03-14 16:12:03 +0800

    mysql

--- examples_mysql/example01.py
+++ examples_mysql/example01.py
@@ -7,7 +7,7 @@ def test_insert():
                            db='liutest', charset='utf8')
     try:
         with conn.cursor() as cursor:
-            result = cursor.execute("insert into t_student values (3,'liuzhengfen','101')")
+            result = cursor.execute("insert into t_student values (1,'liurun','101')")
             if result == 1:
                 print('添加成功')
             conn.commit()
@@ -25,7 +25,7 @@ def test_del():
                            db='liutest', charset='utf8')
     try:
         with conn.cursor() as cursor:
-            result = cursor.execute("delete from t_student where no=%s", (no, ))
+            result = cursor.execute("delete from t_student where no=%s", (no,))
             if result == 1:
                 print('删除成功')
             conn.commit()
@@ -36,5 +36,24 @@ def test_del():
         conn.close()
 
 
+def test_select():
+    t_select_list=[]
+    cno = input('请输入需查询班级编号:')
+    conn = pymysql.connect(host='192.168.226.204', port=3306,
+                           user='root', password='liu0325',
+                           db='liutest', charset='utf8')
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("select no,name,cno from t_student where cno=%s",(cno, ))
+            print(cursor.fetchone())
+            for row in cursor.fetchall():
+                t_select_list.append({"no":row[0],"name":row[1],"con":row[2]})
+            print(t_select_list)
+    except pymysql.MySQLError as error:
+        print(error)
+    finally:
+        conn.close()
+
+
 if __name__ == '__main__':
-    test_del()
+    test_select()
